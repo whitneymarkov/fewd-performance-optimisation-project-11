c49f477c8a46410e00e1a4e1768d226314774ab3
Whitney <whitneymarkov@outlook.com>
2017-09-08 23:54:43 +0200

    css to sass, replaced files w/CDNs, style changes

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).foundation();
+
+
+const imageGrid = document.querySelector('.image-grid');
+let footer = document.querySelector('footer');
+
+imageGrid.addEventListener('click', (e)=>{
+  e.preventDefault();
+  let modalID = e.target.parentElement.getAttribute('data-reveal-id');
+  let alt = e.target.getAttribute('alt');
+  let src = e.target.getAttribute('src');
+  let source = src.replace('.jpg', '-large.jpg');
+  let avatar = e.target.parentElement.nextElementSibling.firstElementChild.getAttribute('src');
+
+  let modalHTML = `
+  <div id="${modalID}" class="reveal-modal" data-reveal aria-hidden="true" role="dialog">
+    <h2 class="modalTitle">Photo</h2>
+    <img src="${source}" alt="${alt}" >
+      <div class="info">
+        <img src="${avatar}" alt="Photo"  class="avatar">
+          <span class="attribution">Photo by <b>Werner Bechtelar</b></span>
+        </div>
+        <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+      </div>`;
+
+      footer.insertAdjacentHTML('afterend', modalHTML);
+
+      $('#'+ modalID).foundation('reveal', 'open');
+  });
